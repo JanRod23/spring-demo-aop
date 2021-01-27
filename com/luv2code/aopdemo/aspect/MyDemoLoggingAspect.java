b1c828e588f0846934aff1df683c7ea1f934a72c
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-27 18:49:23 -0500

    Added advice class to test @Before annotation

--- com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,21 @@
 package com.luv2code.aopdemo.aspect;
 
-public class MyDemoLoggingAspect {
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	// This is where we add all of our related advices for logging
+	
+	// Lets start with a @Before advice
+	
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n==========>> Executing @Before advice on addAccount()");
+		
+	}
 }
