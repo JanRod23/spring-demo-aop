af68f3722fa80f6878fcb69912ca47b82d2a5df7
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-28 12:25:37 -0500

    Added more classes to test Advice annotations

--- com/luv2code/aopdemo/MainDemoApp.java
+++ com/luv2code/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -15,8 +16,12 @@ public class MainDemoApp {
 		// Get the bean from Spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
+		// Get Membership bean from Spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
 		// Call the business method
 		theAccountDAO.addAccount();
+		theMembershipDAO.addAccount();
 		
 		// Close the context
 		context.close();
